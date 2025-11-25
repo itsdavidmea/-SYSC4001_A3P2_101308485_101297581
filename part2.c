fcc2aab318973a420c56e7ca8c866e350c002953
david <mea.david29@outlook.com>
2025-11-24 22:39:48 -0500

    created 20 exam files and rubric

---
+++ part2.c
@@ -0,0 +1,16 @@
+//pass the number of process to main function when startign 
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        // Check if the number of arguments is correct
+        return 1; // Return error code if not
+    }
+
+    int numProcesses = atoi(argv[1]); // Convert argument to integer
+
+    // Rest of the code to create and manage processes
+
+    return 0; 
+}
